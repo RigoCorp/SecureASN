bf57bc33a1a7df6b84c82f1898b3aa368180be25
Marcos Del Sol Vives <marcos@orca.pet>
2023-07-11 11:20:34 +0200

    Filter AS by company name

--- build-ip-list.py
+++ build-ip-list.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python3
 
-import csv
+from collections import namedtuple
+from csv import DictReader
+from io import StringIO
 import requests
+import sys
 
+# Step 1 - read list of secure AS from the .txt file
+AS = namedtuple('AS', ['name', 'asns'])
+wanted_as = dict()
+with open('secure-as.txt') as as_file:
+	for system in DictReader(as_file, delimiter=';', skipinitialspace=True):
+		wanted_as[system['company']] = AS(system['name'], [])
+
+# Step 2 - find all numbers for each company
+as_list = requests.get('https://raw.githubusercontent.com/ipverse/asn-info/master/as.csv').text
+for entry in DictReader(StringIO(as_list)):
+	match = wanted_as.get(entry['description'])
+	if match is not None:
+		match.asns.append(int(entry['asn']))
+
+# Fetch IPs for each one
 with \
-		open('secure-asn.tsv') as tsv_file, \
 		open('ipv4-whitelist.txt', 'w') as ipv4_file, \
 		open('ipv6-whitelist.txt', 'w') as ipv6_file:
-	for system in csv.DictReader(tsv_file, dialect='excel-tab'):
-		print(f"{system['asn']} - {system['name']}")
+	for system in wanted_as.values():
+		print(system.name)
+		for file in [ipv4_file, ipv6_file]:
+			file.write(f'#\n')
+			file.write(f'# {system.name}\n')
+			file.write(f'#\n')
 
-		# Fetch AS information from IPverse AS IP repo
-		system_info = requests.get(f"https://raw.githubusercontent.com/ipverse/asn-ip/master/as/{system['asn']}/aggregated.json")
-		system_info = system_info.json()
+		for asn in system.asns:
+			print(f' - {asn}')
 
-		for file, ipver in [(ipv4_file, 'ipv4'), (ipv6_file, 'ipv6')]:
-			file.write(f"# AS {system['asn']} - {system['name']}\n")
+			# Fetch AS information from IPverse AS IP repo
+			system_info = requests.get(f"https://raw.githubusercontent.com/ipverse/asn-ip/master/as/{asn}/aggregated.json")
+			if system_info.status_code == 200:
+				system_info = system_info.json()
 
-			ip_list = '\n'.join(system_info['subnets'][ipver])
-			if ip_list != '':
-				file.write(ip_list)
-			else:
-				file.write('# (none)')
+				for file, ipver in [(ipv4_file, 'ipv4'), (ipv6_file, 'ipv6')]:
+					file.write(f"# ASN {asn}\n")
 
-			file.write('\n\n')
+					ip_list = '\n'.join(system_info['subnets'][ipver])
+					if ip_list != '':
+						file.write(ip_list)
+					else:
+						file.write('# (none)')
+
+					file.write('\n\n')
+			elif system_info.status_code != 404:
+				system_info.raise_for_status()
